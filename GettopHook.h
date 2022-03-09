d3f0e536f4a86e6cea38f0a370babbb46aa4a8de
final <63569156+FinalLabs@users.noreply.github.com>
2022-03-09 10:07:14 -0500

    Update GettopHook.h

--- GettopHook.h
+++ GettopHook.h
@@ -2,16 +2,16 @@
 #include <iostream>
 #include "detours.h"
 
-int x(int addy) {
-	return (addy - 0x400000 + reinterpret_cast<int>(GetModuleHandleA("RobloxPlayerBeta.exe")));
+__forceinline int x(int addy) {
+	return (addy - 0x400000 + reinterpret_cast<int>(GetModuleHandleA("Process.exe")));
 }
 
 namespace Gettop {
 	uintptr_t r_lua_state = 0;
 
-	int __cdecl sub_154D6F0(int a1) { //gettop lol 
+	int __cdecl gettop_detour(int a1) { //gettop lol 
 		r_lua_state = a1;
-		return (*(DWORD*)(a1 + 20) - *(DWORD*)(a1 + 24)) >> 4;
+		return (*(DWORD*)(a1 + top) - *(DWORD*)(a1 + base)) >> 4;
 	}
 
 	int GettopAddr = x(0x1526A90);
@@ -19,7 +19,7 @@ namespace Gettop {
 	int GetRState() {
 		DetourTransactionBegin();
 		DetourUpdateThread(GetCurrentThread());
-		DetourAttach(&reinterpret_cast<void*&>(GettopAddr), &sub_154D6F0);
+		DetourAttach(&reinterpret_cast<void*&>(GettopAddr), &gettop_detour);
 		DetourTransactionCommit();
 	
 		while (r_lua_state == 0) {
@@ -28,7 +28,7 @@ namespace Gettop {
 
 		DetourTransactionBegin();
 		DetourUpdateThread(GetCurrentThread());
-		DetourDetach(&reinterpret_cast<void*&>(GettopAddr), &sub_154D6F0);
+		DetourDetach(&reinterpret_cast<void*&>(GettopAddr), &gettop_detour);
 		DetourTransactionCommit();
 
 		return r_lua_state;
